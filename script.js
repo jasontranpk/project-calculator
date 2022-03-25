58455e20c92adde7a2afcb568b1fd0859f75996d
Jason <jasontran.pk@gmail.com>
2022-03-25 12:48:21 +0700

    add operator function

---
+++ script.js
@@ -0,0 +1,28 @@
+function add(num1, num2){
+    return num1 + num2;
+}
+function subtract(num1, num2){
+    return num1 - num2;
+}
+function multiply(num1, num2){
+    return num1 * num2;
+}
+function divide(num1, num2){
+    return num1 / num2;
+}
+function operator(symbol, num1, num2){
+    switch (symbol){
+        case '+':
+            return add(num1, num2);
+            break;
+        case '-':
+            return subtract(num1, num2);
+            break;
+        case '*':
+            return multiply(num1, num2);
+            break;
+        case '/':
+            return divide(num1, num2);
+            break;
+    }
+}
